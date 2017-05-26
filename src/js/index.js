2c9fdbfd2b3a3ef2478fd8305b2bb43b85972504
renzhao <renzhao@vpgame.cn>
2017-05-26 13:13:36 +0800

    测试闪烁

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,42 @@
-for (let i = 0; i < 6; i++) {
-  console.log(i)
+const data = {
+    limit: 10,
+    a: 'aa',
+    s: 'ss',
+    b: 'bb',
+    1: '1',
+    11: '11',
+    2: '2',
+    0: 0,
+    haha: null
 }
 
-class bb {
-  constructor() {
-    this.name = 'renzhao'
-  }
+function sortObjString(obj) {
+    if (!Object.keys) {
+        Object.keys = function (obj) {
+            var keys = [];
 
-  handleClick() {
-    console.log('click')
-  }
+            for (var i in obj) {
+                if (obj.hasOwnProperty(i)) {
+                    keys.push(i);
+                }
+            }
+
+            return keys;
+        };
+    }
+    var newKey = Object.keys(obj).sort();
+    var newObj = {};
+    var str = '';
+    for (var i = 0; i < newKey.length; i++) {
+        newObj[newKey[i]] = obj[newKey[i]]
+    }
+    for (var key in newObj) {
+        if (!!newObj[key] || newObj[key] === 0) {
+            str += newObj[key]
+        }
+    }
+    return str;
 }
+
+var res = sortObjString(data)
+console.log(res)
